f66803ebb02fa29f17ce770328c7a4292ffc8b66
Furkan Kalkan <furkan.kalkan3@gmail.com>
2017-04-15 07:02:52 +0300

    eski sürümden kalma gereksiz js kütüphaneleri kaldırıldı

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
-    <script src="xml2json.js"></script>
     <style>
         h4{
             color:#0fadc0;
