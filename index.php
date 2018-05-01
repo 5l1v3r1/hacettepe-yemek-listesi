a8958f1a59c3cf1e64a80a458db2e447d3eac174
Furkan Kalkan <furkan.kalkan3@gmail.com>
2018-05-01 15:37:09 +0300

    Sadece bugünün yemek listesini gösterme hatası fixed

--- index.php
+++ index.php
@@ -25,92 +25,99 @@
             </div>
 
             <div class="card-deck">
-                <?php
-            date_default_timezone_set('Europe/Istanbul');
-            $bugun = new DateTime(date("d.m.Y"));
-            $apiURL = 'http://www.sksdb.hacettepe.edu.tr/YemekListesi.xml';
-            $sahurDosya = "sahur.".$bugun->format('Y').".xml";
-            $yemekListesi = file_get_contents($apiURL);
-            $sahurListesi = file_get_contents($sahurDosya);
-            $xmlDataYemek = simplexml_load_string($yemekListesi);
-            $xmlDataSahur = simplexml_load_string($sahurListesi);
-            $kahvalti = ["Pazartesi" => ["Çay-süt", "Sade Poğaça", "Patates Kızartması-Sosis", "Beyaz Peynir", "Siyah Zeytin", "Tereyağ", "Reçel", "Fındık Ezmesi", "Domates-Biber"], "Salı" => ["Çay", "Patatesli Kol Böreği", "Haşlanmış Yumurta", "Poşet Beyaz Peynir", "Yeşil Zeytin", "Tereyağ", "Bal", "Domates-Salatalık"], "Çarşamba" => ["Çay-süt", "Kek", "Menemen*Patatesli Yumurta", "Üçgen Peynir", "Siyah Zeytin", "Tereyağ", "Reçel", "Domates-Salatalık"], "Perşembe" => ["Çay-meyvesuyu", "Peynirli Tepsi Böreği", "Haşlanmış Yumurta", "Kaşar Peyniri", "Yeşil Zeytin", "Tereyağ", "Bal", "Domates-Biber"], "Cuma" => ["Çay", "Simit", "Beyaz Peynir", "Üçgen Peynir", "Siyah Zeytin", "Tereyağ", "Reçel", "Fındık Ezmesi", "Domates-Biber"], "Cumartesi" => ["Çay-süt", "Gözleme", "Haşlanmış Yumurta", "Beyaz Peynir", "Siyah Zeytin", "Tereyağ", "Bal", "Domates-Salatalık"], "Pazar" => ["Çay-süt", "Kaşarlı-sucuklu Tost", "Menemen", "Poşet Beyaz Peynir", "Yeşil Zeytin", "Tereyağ", "Reçel", "Fındık Ezmesi", "Domates-Biber"]];
-            $kumanya = ["Pazartesi" => ["Kaşar Peynirli Sandviç", "Ayran", "Mevsim Meyvesi"], "Salı" => ["Konserve Barbunya Pilaki*Fasulye Pilaki", "Meyve Suyu", "Mevsim Meyvesi", "Rol Ekmek (2 adet)"], "Çarşamba" => ["Beyaz Peynirli Sandviç", "Ayran", "Mevsim Meyvesi"], "Perşembe" => ["Konserve Ton Balığı*Barbunya Pilaki", "Meyve Suyu", "Mevsim Meyvesi", "Rol Ekmek (2 adet)"], "Cuma" => ["Kaşar Peynirli Sandviç", "Ayran", "Mevsim Meyvesi"], "Cumartesi" => [""], "Pazar" => [""]];
-            foreach($xmlDataYemek->children() as $gun)
-            {
-                if (new DateTime(substr($gun->tarih, 0, 10)) >= $bugun)
-                {
-            ?>
-                    <div class="card text-white bg-dark mb-3" style="min-width: 20rem;">
-                        <div class="card-body">
-                            <h4 class="card-title"><i class="fa fa-calendar" aria-hidden="true"></i> <?php echo $gun->tarih; ?> </h4>
-                            <div class="alert alert-dark" role="alert">
-                                <h4>Kahvaltı</h4>
-                                <ul class="list-group list-group-flush">
-                                    <?php
-                                        $gunAdi = explode(" ", $gun->tarih) [1];
-                                        foreach($kahvalti[$gunAdi] as $yemek)
-                                        {
-                                            echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
-                                        }
-                                    ?>
-                                </ul>
-                            </div>
-                            <div class="alert alert-dark" role="alert">
-                                <h4>Ana Yemek</h4>
-                                <ul class="list-group list-group-flush">
-                                    <?php
-                                        foreach($gun->yemekler->yemek as $yemek)
-                                        {
-                                            echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
-                                        }
+		        <?php
+		            date_default_timezone_set('Europe/Istanbul');
+		            $bugun = new DateTime(date("d.m.Y"));
+		            $apiURL = 'http://www.sksdb.hacettepe.edu.tr/YemekListesi.xml';
+		            $sahurDosya = "sahur.".$bugun->format('Y').".xml";
+		            $yemekListesi = file_get_contents($apiURL);
+		            $sahurListesi = file_get_contents($sahurDosya);
+		            $xmlDataYemek = simplexml_load_string($yemekListesi);
+		            $xmlDataSahur = simplexml_load_string($sahurListesi);
+		            $kahvalti = ["Pazartesi" => ["Çay-süt", "Sade Poğaça", "Patates Kızartması-Sosis", "Beyaz Peynir", "Siyah Zeytin", "Tereyağ", "Reçel", "Fındık Ezmesi", "Domates-Biber"], "Salı" => ["Çay", "Patatesli Kol Böreği", "Haşlanmış Yumurta", "Poşet Beyaz Peynir", "Yeşil Zeytin", "Tereyağ", "Bal", "Domates-Salatalık"], "Çarşamba" => ["Çay-süt", "Kek", "Menemen*Patatesli Yumurta", "Üçgen Peynir", "Siyah Zeytin", "Tereyağ", "Reçel", "Domates-Salatalık"], "Perşembe" => ["Çay-meyvesuyu", "Peynirli Tepsi Böreği", "Haşlanmış Yumurta", "Kaşar Peyniri", "Yeşil Zeytin", "Tereyağ", "Bal", "Domates-Biber"], "Cuma" => ["Çay", "Simit", "Beyaz Peynir", "Üçgen Peynir", "Siyah Zeytin", "Tereyağ", "Reçel", "Fındık Ezmesi", "Domates-Biber"], "Cumartesi" => ["Çay-süt", "Gözleme", "Haşlanmış Yumurta", "Beyaz Peynir", "Siyah Zeytin", "Tereyağ", "Bal", "Domates-Salatalık"], "Pazar" => ["Çay-süt", "Kaşarlı-sucuklu Tost", "Menemen", "Poşet Beyaz Peynir", "Yeşil Zeytin", "Tereyağ", "Reçel", "Fındık Ezmesi", "Domates-Biber"]];
+		            $kumanya = ["Pazartesi" => ["Kaşar Peynirli Sandviç", "Ayran", "Mevsim Meyvesi"], "Salı" => ["Konserve Barbunya Pilaki*Fasulye Pilaki", "Meyve Suyu", "Mevsim Meyvesi", "Rol Ekmek (2 adet)"], "Çarşamba" => ["Beyaz Peynirli Sandviç", "Ayran", "Mevsim Meyvesi"], "Perşembe" => ["Konserve Ton Balığı*Barbunya Pilaki", "Meyve Suyu", "Mevsim Meyvesi", "Rol Ekmek (2 adet)"], "Cuma" => ["Kaşar Peynirli Sandviç", "Ayran", "Mevsim Meyvesi"], "Cumartesi" => [""], "Pazar" => [""]];
 
-                                        echo '<br><b>' . $gun->kalori . ' Kalori</b>';
-                                    ?>
-                                </ul>
-                            </div>
-                            <div class="alert alert-dark" role="alert">
-                                <h4>Kumanya</h4>
-                                <ul class="list-group list-group-flush">
-                                    <?php
-                                        $gunAdi = explode(" ", $gun->tarih) [1];
-                                        foreach($kumanya[$gunAdi] as $yemek)
-                                        {
-                                            if ($yemek)
-                                            {
-                                                echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
-                                            }
-                                            else
-                                            {
-                                                echo "<i>Bugün kumanya menü hizmeti verilmemektedir.</i>";
-                                            }
-                                        }
-                                    ?>
-                                </ul>
-                            </div>
-                            <?php 
-                        $gunler = array(1=>"Pazartesi",2=>"Salı",3=>"Çarşamba",4=>"Perşembe",5=>"Cuma",6=>"Cumartesi",8=>"Pazar");
-                        $gun = $gunler[$bugun -> format("w")];
-                        $sahur = $xmlDataSahur->xpath('//gunler/gun/tarih[.="'.$bugun->format("d.m.Y").' '.$gun.'"]/parent::*');
-                        if ($sahur) {
-                        ?>
-                                <div class="alert alert-dark" role="alert">
-                                    <h4>Sahur</h4>
-                                    <ul class="list-group list-group-flush">
-                                        <?php
-                                        foreach($sahur[0]->yemekler->yemek as $yemek){
-                                            echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
+		            foreach($xmlDataYemek->children() as $gun)
+		            {
+		            	
+		           	   	if (new DateTime(substr($gun->tarih, 0, 10)) >= $bugun)
+		                {
+		            ?>
+		                    <div class="card text-white bg-dark mb-3" style="min-width: 20rem;">
+		                        <div class="card-body">
+		                            <h4 class="card-title"><i class="fa fa-calendar" aria-hidden="true"></i> <?php echo $gun->tarih; ?> </h4>
+		                            <div class="alert alert-dark" role="alert">
+		                                <h4>Kahvaltı</h4>
+		                                <ul class="list-group list-group-flush">
+		                                    <?php
+		                                        $gunAdi = explode(" ", $gun->tarih) [1];
+		                                        foreach($kahvalti[$gunAdi] as $yemek)
+		                                        {
+		                                            echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
+		                                        }
+		                                    ?>
+		                                </ul>
+		                            </div>
+		                            <div class="alert alert-dark" role="alert">
+		                                <h4>Ana Yemek</h4>
+		                                <ul class="list-group list-group-flush">
+		                                    <?php
+		                                        foreach($gun->yemekler->yemek as $yemek)
+		                                        {
+		                                            echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
+		                                        }
 
-                                        }
-                                    }
-                                ?>
-                                    </ul>
-                                </div>
-                        </div>
-                        <?php
-                                }
-                            }
-                        ?>
+		                                        echo '<br><b>' . $gun->kalori . ' Kalori</b>';
+		                                    ?>
+		                                </ul>
+		                            </div>
+		                            <div class="alert alert-dark" role="alert">
+		                                <h4>Kumanya</h4>
+		                                <ul class="list-group list-group-flush">
+		                                    <?php
+		                                        $gunAdi = explode(" ", $gun->tarih) [1];
+		                                        foreach($kumanya[$gunAdi] as $yemek)
+		                                        {
+		                                            if ($yemek)
+		                                            {
+		                                                echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
+		                                            }
+		                                            else
+		                                            {
+		                                                echo "<i>Bugün kumanya menü hizmeti verilmemektedir.</i>";
+		                                            }
+		                                        }
+		                                    ?>
+		                                </ul>
+		                            </div>
+
+		                            <?php 
+										if ($xmlDataSahur) {
+											$tarih = rtrim($gun->tarih);
+		                        			$sahur = $xmlDataSahur->xpath('//gunler/gun/tarih[.="'.$tarih.'"]/parent::*');
+		                        			if ($sahur) {
+		                       		?>
+		                            <div class="alert alert-dark" role="alert">
+		                                <h4>Sahur</h4>
+		                                <ul class="list-group list-group-flush">
+		                                <?php
+		                                    foreach($sahur[0]->yemekler->yemek as $yemek){
+		                                        echo '<li class="list-group-item list-group-item-dark text-white">'.$yemek.'</li>';
+		                                    }
+		                  				?>	
+
+		                                </ul>
+		                            </div>
+		                            <?php 
+		                        		}
+		                        		   }
+		                         	?>	
+		                        </div>
+		                    </div>
+		           	<?php
+		                    }
+		                }
+		            ?>
                     </div>
             </div>
     </body>
